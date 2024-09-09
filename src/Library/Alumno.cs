eeb194902bf248b8a181c6ec68a752141c2dc704
Avril Viega <avrilviega@gmail.com>
2024-09-09 19:44:54 -0300

    Inicializando el proyecto

---
+++ src/Library/Alumno.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace Library;
+
+public class Alumno
+{
+    public string Nombre { get; set; }
+    public DateTime FechaNacimiento { get; set; }
+    public string Licencia { get; set; }
+
+    public Alumno(string nombre, DateTime fechaNacimiento, string licencia)
+    {
+        Nombre = nombre;
+        FechaNacimiento = fechaNacimiento;
+        Licencia = licencia;
+    }
+
+    public string GetDetalles()
+    {
+        return $"{Nombre}, Fecha de Nacimiento: {FechaNacimiento.ToShortDateString()}, Licencia: {Licencia}";
+    }
+
+    public void AsignarAuto(Auto auto)
+    {
+        // Lógica para asignar un auto al alumno
+    }
+}
